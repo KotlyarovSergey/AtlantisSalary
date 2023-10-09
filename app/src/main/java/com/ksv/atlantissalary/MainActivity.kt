015baed264283aaaa425216be03a3915f8146f7d
KotlyarovSergey <zews-sv@rambler.ru>
2023-10-09 21:01:22 +0300

    Recaclulation on change values in edits

--- app/src/main/java/com/ksv/atlantissalary/MainActivity.kt
+++ app/src/main/java/com/ksv/atlantissalary/MainActivity.kt
@@ -2,36 +2,85 @@ package com.ksv.atlantissalary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.widget.addTextChangedListener
 import com.ksv.atlantissalary.controller.Controller
 import com.ksv.atlantissalary.databinding.ActivityMainBinding
-import com.ksv.atlantissalary.model.AllWorkedHours
-import com.ksv.atlantissalary.model.NameAmountPair
-import com.ksv.atlantissalary.model.SalaryCalculator
-import com.ksv.atlantissalary.view.Viewer
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var controller : Controller
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         controller = Controller(binding)
         addTextChangedListeners()
 
-        binding.buttonCalculate.setOnClickListener { controller.onChangeInputData() }
+        binding.buttonCalculate.setOnClickListener { inputDataChanged() }
+
 
     }
 
      private fun addTextChangedListeners() {
-        binding.editTextSalaryGrade.addTextChangedListener { controller.onChangeInputData() }
-        binding.editTextTotalHours.addTextChangedListener { controller.onChangeInputData() }
-        binding.editTextNightHours.addTextChangedListener { controller.onChangeInputData() }
-        binding.editTextDaysOvershifts.addTextChangedListener { controller.onChangeInputData() }
-        binding.editTextNightOvershifts.addTextChangedListener { controller.onChangeInputData() }
+
+         var grade = binding.editTextSalaryGrade.text.toString()
+         binding.editTextSalaryGrade.setOnFocusChangeListener { view, hasFocus ->
+             if (!hasFocus){
+                 if (binding.editTextSalaryGrade.text.toString() != grade)
+                     inputDataChanged()
+             }else{
+                 grade = binding.editTextSalaryGrade.text.toString()
+             }
+         }
+
+         var totalHours = binding.editTextTotalHours.text.toString()
+         binding.editTextTotalHours.setOnFocusChangeListener { view, hadFocus ->
+             if(hadFocus){
+                 totalHours = binding.editTextTotalHours.text.toString()
+             } else{
+                 if(totalHours != binding.editTextTotalHours.text.toString())
+                     inputDataChanged()
+             }
+         }
+
+         var  nightHours = binding.editTextNightHours.text.toString()
+         binding.editTextNightHours.setOnFocusChangeListener { view, hadFocus ->
+             if(hadFocus){
+                 nightHours = binding.editTextNightHours.text.toString()
+             } else{
+                 if(nightHours != binding.editTextNightHours.text.toString())
+                     inputDataChanged()
+             }
+
+         }
+
+         var dayOvershifts = binding.editTextDaysOvershifts.text.toString()
+         binding.editTextDaysOvershifts.setOnFocusChangeListener { view, hadFocus ->
+             if(hadFocus){
+                 dayOvershifts = binding.editTextDaysOvershifts.text.toString()
+             }else{
+                 if(dayOvershifts != binding.editTextDaysOvershifts.text.toString())
+                     inputDataChanged()
+             }
+         }
+
+         var nightOvershifts = binding.editTextNightOvershifts.text.toString()
+         binding.editTextNightOvershifts.setOnFocusChangeListener { view, hadFocus ->
+             if(hadFocus){
+                 nightOvershifts = binding.editTextNightOvershifts.text.toString()
+             }else{
+                 if(nightOvershifts != binding.editTextNightOvershifts.text.toString())
+                     inputDataChanged()
+             }
+
+         }
+    }
+
+    private fun inputDataChanged(){
+        controller.onChangeInputData()
     }
 
 }
